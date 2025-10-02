d470752843dd69a34151652bf77825a6b69013f0
Sergii Grytsaienko <sergiigrytsaienko@gmail.com>
2025-10-02 16:33:51 -0500

    flight-ticket-detail

---
+++ 8. flight-ticket-detail/code.jsx
@@ -0,0 +1,201 @@
+import imgImgLogo from "./images/7cf78ddc6a247e15bf19de8c8f5cbf913d75128a.png";
+import imgIcBack from "./images/e16403343626fc30c4d5c92a8259a35cf2e1a83b.svg";
+import imgDivider from "./images/7ae070cfdfb443a8494cb081f8dd85bf4fbd88bd.svg";
+import imgPoint from "./images/415346bbcd3cbce5d47353976a9ffed8fcbfdae2.svg";
+import imgDivider1 from "./images/b81505ab1fb62a7c06f49ffcb5686f01b88e8ef1.svg";
+import imgPoint1 from "./images/898f94422635dec04b35b3cc529f578fbc7470bb.svg";
+import imgIcPlane from "./images/a5694d15c04a5bbcf918f094011afcb628e5ea7e.svg";
+import imgBarcode from "./images/2302e736b50fb6f7d762786b19fdddc1a199568b.svg";
+import imgNetworkSignalLight from "./images/46823cc49f385a39a180d4607bd78b50500ede52.svg";
+import imgWiFiSignalLight from "./images/f27a156fde9ca0317ce34ba27116d5c10ead46e3.svg";
+import imgBatteryLight from "./images/f6609037bb671b83b7638342ff758f6cbcc09893.svg";
+import imgIndicator from "./images/bebb99a14926b97882cbc50d061b2bb668daa005.svg";
+import img941 from "./images/7e44c2049b7cb27ff9c88c4e3d7b45d2a3c2e531.svg";
+
+export default function FlightTicketDetail() {
+  return (
+    <div className="bg-white relative size-full" data-name="Flight Ticket Detail" data-node-id="1:657">
+      <div className="absolute bg-[#1c6ae4] box-border content-stretch flex gap-[8px] h-[48px] items-center left-[24px] px-[16px] py-[12px] rounded-[2px] top-[740px] w-[327px]" data-name="cta-download" data-node-id="1:658">
+        <p className="basis-0 font-['General_Sans:Semibold',_sans-serif] grow leading-[22px] min-h-px min-w-px not-italic relative shrink-0 text-[14px] text-center text-white" data-node-id="1:659">
+          Download e-Ticket
+        </p>
+      </div>
+      <div className="absolute box-border content-stretch flex items-center justify-center left-0 overflow-clip px-0 py-[24px] top-[44px] w-[375px]" data-name="Section" data-node-id="1:660">
+        <div className="content-stretch flex items-start overflow-clip relative shrink-0" data-name="Container" data-node-id="1:661">
+          <div className="content-stretch flex flex-col gap-[24px] items-center overflow-clip relative shrink-0" data-name="Content" data-node-id="1:662">
+            <div className="content-stretch flex gap-[12px] items-center relative shrink-0 w-[327px]" data-name="top-bar" data-node-id="1:663">
+              <div className="relative shrink-0 size-[24px]" data-name="ic-back" data-node-id="1:664">
+                <img alt="" className="block max-w-none size-full" src={imgIcBack} />
+              </div>
+              <p className="basis-0 font-['General_Sans:Medium',_sans-serif] grow leading-[24px] min-h-px min-w-px not-italic relative shrink-0 text-[#141511] text-[16px] text-center" data-node-id="1:667">
+                e-Ticket
+              </p>
+            </div>
+            <div className="bg-white box-border content-stretch flex flex-col gap-[24px] items-center justify-center p-[16px] relative rounded-[2px] shrink-0 w-full" data-name="ticket-detail" data-node-id="1:668">
+              <div aria-hidden="true" className="absolute border border-[#eef0eb] border-solid inset-0 pointer-events-none rounded-[2px]" />
+              <div className="content-stretch flex gap-[16px] items-center relative shrink-0 w-full" data-name="logo-airline" data-node-id="1:669">
+                <div className="basis-0 content-stretch flex flex-col gap-[4px] grow items-start min-h-px min-w-px relative shrink-0" data-name="logo" data-node-id="1:670">
+                  <div className="h-[50px] relative shrink-0 w-[81.594px]" data-name="img-logo" data-node-id="1:671">
+                    <img alt="" className="absolute inset-0 max-w-none object-50%-50% object-cover pointer-events-none size-full" src={imgImgLogo} />
+                  </div>
+                </div>
+                <div className="basis-0 content-stretch flex flex-col gap-[4px] grow items-start min-h-px min-w-px not-italic relative shrink-0 text-right" data-name="airline" data-node-id="1:672">
+                  <p className="font-['General_Sans:Medium',_sans-serif] leading-[24px] relative shrink-0 text-[#141511] text-[16px] w-full" data-node-id="1:673">
+                    Garuda Indonesia
+                  </p>
+                  <p className="font-['General_Sans:Regular',_sans-serif] leading-[22px] relative shrink-0 text-[#8c8d89] text-[14px] w-full" data-node-id="1:674">
+                    Economy
+                  </p>
+                </div>
+              </div>
+              <div className="h-0 relative shrink-0 w-full" data-name="divider" data-node-id="1:675">
+                <div className="absolute bottom-0 left-0 right-0 top-[-1px]">
+                  <img alt="" className="block max-w-none size-full" src={imgDivider} />
+                </div>
+              </div>
+              <div className="content-stretch flex items-center relative shrink-0 w-full" data-name="flight-details" data-node-id="1:676">
+                <div className="basis-0 content-stretch flex flex-col gap-[4px] grow items-start min-h-px min-w-px not-italic relative shrink-0" data-name="departure" data-node-id="1:677">
+                  <p className="font-['General_Sans:Regular',_sans-serif] leading-[22px] relative shrink-0 text-[#8c8d89] text-[14px] w-full" data-node-id="1:678">
+                    Jakarta (CGK)
+                  </p>
+                  <p className="font-['General_Sans:Medium',_sans-serif] leading-[24px] relative shrink-0 text-[#141511] text-[16px] w-full" data-node-id="1:679">
+                    12:00 PM
+                  </p>
+                  <p className="font-['General_Sans:Regular',_sans-serif] leading-[20px] relative shrink-0 text-[#8c8d89] text-[12px] w-full" data-node-id="1:680">
+                    24 Feb 2023
+                  </p>
+                </div>
+                <div className="basis-0 content-stretch flex flex-col gap-[4px] grow items-end min-h-px min-w-px not-italic relative shrink-0 text-right" data-name="arrival" data-node-id="1:681">
+                  <p className="font-['General_Sans:Regular',_sans-serif] leading-[22px] relative shrink-0 text-[#8c8d89] text-[14px] w-full" data-node-id="1:682">
+                    Yogyakarta (YIA)
+                  </p>
+                  <p className="font-['General_Sans:Medium',_sans-serif] leading-[24px] relative shrink-0 text-[#141511] text-[16px] w-full" data-node-id="1:683">
+                    01:15 PM
+                  </p>
+                  <p className="font-['General_Sans:Regular',_sans-serif] leading-[20px] relative shrink-0 text-[#8c8d89] text-[12px] w-full" data-node-id="1:684">
+                    24 Feb 2023
+                  </p>
+                </div>
+                <div className="absolute bottom-[-6px] content-stretch flex flex-col gap-[16px] items-center left-1/2 translate-x-[-50%] w-[121px]" data-name="duration" data-node-id="1:685">
+                  <div className="content-stretch flex items-center relative shrink-0 w-full" data-name="point-group" data-node-id="1:686">
+                    <div className="relative shrink-0 size-[6px]" data-name="point" data-node-id="1:687">
+                      <img alt="" className="block max-w-none size-full" src={imgPoint} />
+                    </div>
+                    <div className="basis-0 grow h-0 min-h-px min-w-px relative shrink-0" data-name="divider" data-node-id="1:688">
+                      <div className="absolute bottom-0 left-0 right-0 top-[-1px]">
+                        <img alt="" className="block max-w-none size-full" src={imgDivider1} />
+                      </div>
+                    </div>
+                    <div className="relative shrink-0 size-[6px]" data-name="point" data-node-id="1:689">
+                      <img alt="" className="block max-w-none size-full" src={imgPoint1} />
+                    </div>
+                    <div className="absolute size-[20px] top-1/2 translate-x-[-50%] translate-y-[-50%]" data-name="ic-plane" data-node-id="1:690" style={{ left: "calc(50% - 0.5px)" }}>
+                      <img alt="" className="block max-w-none size-full" src={imgIcPlane} />
+                    </div>
+                  </div>
+                  <p className="font-['General_Sans:Regular',_sans-serif] leading-[20px] not-italic relative shrink-0 text-[#8c8d89] text-[12px] text-center w-full" data-node-id="1:693">
+                    Duration 1h 15m
+                  </p>
+                </div>
+              </div>
+              <div className="h-0 relative shrink-0 w-full" data-name="divider" data-node-id="1:694">
+                <div className="absolute bottom-0 left-0 right-0 top-[-1px]">
+                  <img alt="" className="block max-w-none size-full" src={imgDivider} />
+                </div>
+              </div>
+              <div className="content-stretch flex items-center justify-center relative shrink-0 w-full" data-name="order-details" data-node-id="1:695">
+                <div className="basis-0 content-stretch flex flex-col gap-[12px] grow items-start min-h-px min-w-px relative shrink-0" data-name="detail-group" data-node-id="1:696">
+                  <div className="content-stretch flex flex-col gap-[4px] items-start not-italic relative shrink-0 w-full" data-name="name" data-node-id="1:697">
+                    <p className="font-['General_Sans:Regular',_sans-serif] leading-[22px] relative shrink-0 text-[#8c8d89] text-[14px] w-full" data-node-id="1:698">
+                      Name
+                    </p>
+                    <p className="font-['General_Sans:Medium',_sans-serif] leading-[24px] relative shrink-0 text-[#141511] text-[16px] w-full" data-node-id="1:699">
+                      Cameron Williamson
+                    </p>
+                  </div>
+                  <div className="content-stretch flex flex-col gap-[4px] items-start not-italic relative shrink-0 w-full" data-name="class" data-node-id="1:700">
+                    <p className="font-['General_Sans:Regular',_sans-serif] leading-[22px] relative shrink-0 text-[#8c8d89] text-[14px] w-full" data-node-id="1:701">
+                      Class
+                    </p>
+                    <p className="font-['General_Sans:Medium',_sans-serif] leading-[24px] relative shrink-0 text-[#141511] text-[16px] w-full" data-node-id="1:702">
+                      Economy
+                    </p>
+                  </div>
+                  <div className="content-stretch flex flex-col gap-[4px] items-start not-italic relative shrink-0 w-full" data-name="id-number" data-node-id="1:703">
+                    <p className="font-['General_Sans:Regular',_sans-serif] leading-[22px] relative shrink-0 text-[#8c8d89] text-[14px] w-full" data-node-id="1:704">
+                      ID number
+                    </p>
+                    <p className="font-['General_Sans:Medium',_sans-serif] leading-[24px] relative shrink-0 text-[#141511] text-[16px] w-full" data-node-id="1:705">
+                      1289754724
+                    </p>
+                  </div>
+                </div>
+                <div className="basis-0 content-stretch flex flex-col gap-[12px] grow items-end min-h-px min-w-px relative shrink-0" data-name="detail-group" data-node-id="1:706">
+                  <div className="basis-0 content-stretch flex flex-col gap-[4px] grow items-end min-h-px min-w-px not-italic relative shrink-0 text-right w-full" data-name="booking-code" data-node-id="1:707">
+                    <p className="font-['General_Sans:Regular',_sans-serif] leading-[22px] relative shrink-0 text-[#8c8d89] text-[14px] w-full" data-node-id="1:708">
+                      Booking Code
+                    </p>
+                    <p className="font-['General_Sans:Medium',_sans-serif] leading-[24px] relative shrink-0 text-[#141511] text-[16px] w-full" data-node-id="1:709">
+                      V13NS90
+                    </p>
+                  </div>
+                  <div className="basis-0 content-stretch flex flex-col gap-[4px] grow items-end min-h-px min-w-px not-italic relative shrink-0 text-right w-full" data-name="seat-number" data-node-id="1:710">
+                    <p className="font-['General_Sans:Regular',_sans-serif] leading-[22px] relative shrink-0 text-[#8c8d89] text-[14px] w-full" data-node-id="1:711">
+                      Seat
+                    </p>
+                    <p className="font-['General_Sans:Medium',_sans-serif] leading-[24px] relative shrink-0 text-[#141511] text-[16px] w-full" data-node-id="1:712">
+                      8D
+                    </p>
+                  </div>
+                  <div className="basis-0 content-stretch flex flex-col gap-[4px] grow items-end min-h-px min-w-px not-italic relative shrink-0 text-right w-full" data-name="airline-name" data-node-id="1:713">
+                    <p className="font-['General_Sans:Regular',_sans-serif] leading-[22px] relative shrink-0 text-[#8c8d89] text-[14px] w-full" data-node-id="1:714">
+                      Airline
+                    </p>
+                    <p className="font-['General_Sans:Medium',_sans-serif] leading-[24px] relative shrink-0 text-[#141511] text-[16px] w-full" data-node-id="1:715">
+                      Garuda Indonesia
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="h-0 relative shrink-0 w-full" data-name="divider" data-node-id="1:716">
+                <div className="absolute bottom-0 left-0 right-0 top-[-1px]">
+                  <img alt="" className="block max-w-none size-full" src={imgDivider} />
+                </div>
+              </div>
+              <div className="content-stretch flex flex-col gap-[8px] items-center relative shrink-0 w-full" data-name="barcode" data-node-id="1:717">
+                <div className="h-[64px] relative shrink-0 w-full" data-name="barcode" data-node-id="1:718">
+                  <img alt="" className="block max-w-none size-full" src={imgBarcode} />
+                </div>
+                <p className="font-['General_Sans:Medium',_sans-serif] leading-[22px] not-italic relative shrink-0 text-[#141511] text-[14px] text-center w-full" data-node-id="1:759">
+                  EESBH21200001088
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="absolute h-[44px] left-0 overflow-clip right-0 top-0" data-name="Status Bar" data-node-id="1:760">
+        <div className="absolute h-[30px] left-0 right-0 top-0" data-name="Notch" data-node-id="1:761" />
+        <div className="absolute content-stretch flex gap-[4px] items-center right-[14px] top-[16px]" data-name="Status Icons" data-node-id="1:765">
+          <div className="h-[14px] relative shrink-0 w-[20px]" data-name="Network Signal / Light" data-node-id="1:766">
+            <img alt="" className="block max-w-none size-full" src={imgNetworkSignalLight} />
+          </div>
+          <div className="h-[14px] relative shrink-0 w-[16px]" data-name="WiFi Signal / Light" data-node-id="1:775">
+            <img alt="" className="block max-w-none size-full" src={imgWiFiSignalLight} />
+          </div>
+          <div className="h-[14px] relative shrink-0 w-[25px]" data-name="Battery / Light" data-node-id="1:779">
+            <img alt="" className="block max-w-none size-full" src={imgBatteryLight} />
+          </div>
+        </div>
+        <div className="absolute right-[71px] size-[6px] top-[8px]" data-name="Indicator" data-node-id="1:789">
+          <img alt="" className="block max-w-none size-full" src={imgIndicator} />
+        </div>
+        <div className="absolute h-[21px] left-[21px] overflow-clip rounded-[20px] top-[12px] w-[54px]" data-name="Time / Light" data-node-id="1:791">
+          <div className="absolute h-[15px] top-1/2 translate-x-[-50%] translate-y-[-50%] w-[33px]" data-name="9:41" data-node-id="1:792" style={{ left: "calc(50% + 0.5px)" }}>
+            <img alt="" className="block max-w-none size-full" src={img941} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
